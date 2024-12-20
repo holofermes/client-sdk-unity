5aadac7a7a6de0c9b814f31a192be8c39e67ed32
holofermes <holofermes@gmail.com>
2024-12-20 13:46:04 -0500

    Enhance audio source functionality

--- Runtime/Scripts/MicrophoneSource.cs
+++ Runtime/Scripts/MicrophoneSource.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace LiveKit
@@ -10,11 +11,13 @@ namespace LiveKit
         private AudioFilter _audioFilter;
 
         public override bool IsPlaying => source.isPlaying;
+        public override RtcAudioSourceType AudioSourceType => RtcAudioSourceType.AudioSourceMicrophone;
         public override uint Channels => 2;
         public override event Action<float[], int, int> AudioRead;
 
         public AudioSource Source => source;
         private Action<float[], int, int> _audioRead;
+        private string _deviceName;
 
         private void Awake()
         {
@@ -25,6 +28,18 @@ namespace LiveKit
         private void AudioFilterOnAudioRead(float[] data, int channels, int samplerate)
         {
             AudioRead?.Invoke(data, channels, samplerate);
+        }
+
+        public void Configure(string device, bool loop, int lenghtSec, int frequency)
+        {
+            _deviceName = device;
+            Source.clip = Microphone.Start(device, loop, lenghtSec, frequency);
+            Source.loop = true;
+        }
+        
+        public override IEnumerator Prepare(float timeout = 0)
+        {
+            return new WaitUntil(() => Microphone.GetPosition(_deviceName) > 0);
         }
 
         public override void Play()
@@ -38,5 +53,6 @@ namespace LiveKit
             _audioFilter.AudioRead -= AudioFilterOnAudioRead;
             source.Stop();
         }
+
     }
 }
