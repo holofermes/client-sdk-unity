82cd993ea75bc9dde78596f20a07adf4886fc5ba
holofermes <holofermes@gmail.com>
2024-12-20 13:46:04 -0500

    Enhance audio source functionality

--- Runtime/Scripts/BaseAudioSource.cs
+++ Runtime/Scripts/BaseAudioSource.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace LiveKit
@@ -6,8 +7,10 @@ namespace LiveKit
     public abstract class BaseAudioSource : MonoBehaviour
     {
         public abstract event Action<float[], int, int> AudioRead;
+        public abstract RtcAudioSourceType AudioSourceType { get; }
         public abstract uint Channels { get; }
         public abstract bool IsPlaying { get; }
+        public virtual IEnumerator Prepare(float timeout = 0) { yield break;  }
         public abstract void Play();
         public abstract void Stop();
     }
